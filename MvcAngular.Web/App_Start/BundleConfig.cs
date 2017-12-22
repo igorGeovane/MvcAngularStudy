48725f47bb66b18280d67718d0b5e8c074790ceb
igls <igls@NBPITANG-106>
2017-12-22 15:02:15 -0300

    Add project files.

---
+++ MvcAngular.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcAngular.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/Bundles")
+                .Include("~/bundles/inline.*")
+                .Include("~/bundles/polyfills.*")
+                .Include("~/bundles/scripts.*")
+                .Include("~/bundles/vendor.*")
+                .Include("~/bundles/main.*"));
+
+            bundles.Add(new StyleBundle("~/Scripts/Styles")
+                .Include("~/bundles/styles.*"));
+        }
+    }
+}
